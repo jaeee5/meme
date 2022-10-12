6af993f049c7205010fe450c0276031594db6dd9
jaeee5 <elechijason@gmail.com>
2022-10-12 17:22:34 +0100

    final css updates

--- memeGenerator-react/src/App.jsx
+++ memeGenerator-react/src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react"
+import {useState} from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
 
+
 export default function App() {
+
     return (
         <div>
             <Header />
